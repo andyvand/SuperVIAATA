636e394049a4c0b83854ada20f11efa41c9c1dc0
Joris Schepers <joriss@imac.home>
2015-01-18 20:28:30 +0100

    Initial commit

---
+++ Source/build/SuperVIAATA.build/Deployment/SuperVIAATA.build/DerivedSources/SuperVIAATA_vers.c
@@ -0,0 +1,1 @@
+ const unsigned char SuperVIAATAVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:SuperVIAATA  PROJECT:SuperVIAATA-1.0.3" "\n"; const double SuperVIAATAVersionNumber __attribute__ ((used)) = (double)1.0;
---
+++ Source/build/SuperVIAATA.build/Deployment/SuperVIAATA.build/Objects-normal/x86_64/SuperVIAATA_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/joris/Downloads/SuperVIAATA/Source/build/SuperVIAATA.build/Deployment/SuperVIAATA.build/DerivedSources/SuperVIAATA_vers.c
